b7610f1089ca6c0dba979cef53417dbe56e3158f
hlatki <hannahatkinson@gmail.com>
2013-08-15 09:58:02 -0400

    Add tests for hand ranking

--- hatkinson/poker/test/poker/core_test.clj
+++ hatkinson/poker/test/poker/core_test.clj
@@ -2,6 +2,20 @@
   (:require [clojure.test :refer :all]
             [poker.core :refer :all]))
 
+(defn get-rank
+  "helper function to get rank of hand, given hand as string"
+  [hand-string]
+  (first (rank-hand (clojure.string/split hand-string #" "))))
+
+
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Test that hands are classified correctly"
+    (is (get-rank "TS JS QS KS AS") :straight-flush)
+    (is (get-rank "2D 2S 2H 2C KH") :four-of-a-kind)
+    (is (get-rank "2D 2H 2S AH AS") :full-house)
+    (is (get-rank "AH TH 8H 6H JH") :flush)
+    (is (get-rank "6H 7S 8H 9S TS") :straight)
+    (is (get-rank "2D 2S 2H JC AH") :three-of-a-kind)
+    (is (get-rank "3H AC 3D AS KD") :two-pair)
+    (is (get-rank "AH KS AC TS 2H") :pair)
+    (is (get-rank "6H 2C QH 3S TH") :high-card)))
